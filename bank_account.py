6e41a14767e9475f33dabbb25fe5b7a6b2997eb7
ThisIsEmma <sakatia.lise@gmail.com>
2022-10-26 21:27:59 -0400

    completed all methods

--- bank_account.py
+++ bank_account.py
@@ -23,10 +23,23 @@ class BankAccount:
     def get_balance(self):
         print(f'Hello {self.full_name}, your account currently has a balance of: ${"{0:.2f}".format(self.balance)}. Have a wonderful day!')
         return self.balance
+
+    def add_interest(self):
+        monthly_interest_rate = 0.00083
+        interest = self.balance * monthly_interest_rate
+        self.balance += interest
+        
+    def print_statement(self):
+        print(f'{self.full_name}\nAccount No.: ****{self.account_number[4:]}\nBalance: ${self.balance}')
         
 
-myAccount = BankAccount('Edith','0001', 100)
 
-myAccount.deposit(50)
-myAccount.withdraw(500)
-myAccount.get_balance()
+liz_account = BankAccount('Liz Truss','00123457', 1500)
+rishi_account = BankAccount('Rishi Sunak','88978432', 1600)
+royal_account = BankAccount('Charles III','74212378', 5000)
+
+liz_account.deposit(1500)
+royal_account.withdraw(500)
+rishi_account.print_statement()
+liz_account.add_interest()
+liz_account.get_balance()
